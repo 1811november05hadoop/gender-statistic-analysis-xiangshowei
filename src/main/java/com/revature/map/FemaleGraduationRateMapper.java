5e43edc3a25521b8c9e6ae3c1bc6f05ba524909b
xiangshowei <xiangshowei@gmail.com>
2018-12-11 20:17:27 -0500

    added more class constants; output <key, value> is still <Text, DoubleWritable>

--- src/main/java/com/revature/map/FemaleGraduationRateMapper.java
+++ src/main/java/com/revature/map/FemaleGraduationRateMapper.java
@@ -13,7 +13,7 @@ import org.apache.log4j.Logger;
 public class FemaleGraduationRateMapper extends Mapper<LongWritable, Text, Text, DoubleWritable> {
 
 	private static final Logger LOGGER = Logger.getLogger(FemaleGraduationRateMapper.class); 
- 
+
 	private static final int COUNTRY_NAME_COLUMN = 0;
 	private static final int COUNTRY_CODE_COLUMN = 1;
 	private static final int INDICATOR_CODE_COLUMN = 3;
@@ -22,7 +22,9 @@ public class FemaleGraduationRateMapper extends Mapper<LongWritable, Text, Text,
 
 	//Gross graduation ratio, tertiary, female (%)
 	private static final String INDICATOR_CODE = "SE.TER.CMPL.FE.ZS";
-
+	private static final int THRESHOLD = 30;
+	private static final int START_YEAR = 1960;
+	
 	private static final HashSet<String> conglomerateCountryCodes = 
 			new HashSet<>(Arrays.asList("Country Code", 
 					"ARB", "CSS", "CEB", "EAR", "EAS",
@@ -74,69 +76,73 @@ public class FemaleGraduationRateMapper extends Mapper<LongWritable, Text, Text,
 		if(relevantData) {
 			// Country Name is the first column in the data so it doesn't match the regular expression
 			String countryName = row[COUNTRY_NAME_COLUMN].replaceFirst("\"", "");
+			int year = START_YEAR;
 
 			for (int i = START_YEAR_COLUMN; i <= END_YEAR_COLUMN; i++) {
 				String graduationRateInYearStr = row[i];
-				double graduationRateInYear = 0;
-				
-				if (!graduationRateInYearStr.isEmpty()) {
-					graduationRateInYear = Double.parseDouble(graduationRateInYearStr);					
-				}
-				
-				if (graduationRateInYear < 30) 
-					/* pads the output with spaces so it reaches 38 characters for displaying purposes
-					 * NOTE: a tab(\t) counts as 4 spaces in size 
-					 * 
-					 * [java.lang.String]	String.format("%-38s", countryName)
-					 * 		https://docs.oracle.com/javase/7/docs/api/java/lang/String.html#format%28java.lang.String,%20java.lang.Object...%29
-					 * 
-					 * ----------copied from official documentation----------
-					 * 
-					 * Format(ter) String syntax: %[argument_index$][flags][width][.precision]conversion
-					 * 
-					 * [argument_index$] The optional argument_index is a decimal integer indicating the position of the argument in the argument list. 
-					 * 					 The first argument is referenced by "1$", the second by "2$", etc.
-					 * 
-					 * [flags] The optional flags is a set of characters that modify the output format. The set of valid flags depends on the conversion.
-					 * 
-					 * [width] The optional width is a non-negative decimal integer indicating the minimum number of characters to be written to the output.
-					 * 
-					 * [.precision] The optional precision is a non-negative decimal integer usually used to restrict the number of characters. 
-					 * 				The specific behavior depends on the conversion.
-					 * 
-					 * conversion: The required conversion is a character indicating how the argument should be formatted. 
-					 * 		      The set of valid conversions for a given argument depends on the argument's data type. 
-					 * 
-					 * ----------copied from official documentation----------
-					 * 
-					 * 
-					 * 
-					 * System.out.printf syntax: %[flags][width][.precision][argsize]typechar 
-					 * 		https://sharkysoft.com/archive/printf/docs/javadocs/lava/clib/stdio/doc-files/specification.htm
-					 * 
-					 * ----------copied from above link----------
-					 * 
-					 * % indicates that it's a format code  
-					 * [flags] optional prefix; "-" flag left justifies(align left) or pads to the right of the String
-					 * 
-					 * [width] optional "width specifier", if present, indicates the field width, 
-					 * 			or the minimum number of characters in the output that the formatted argument will span. 
-					 * 				-If the string representation of the value does NOT fill the minimum length, the field will be left-padded with spaces. 
-					 * 				-If the converted value exceeds the minimum length, however, the converted result will NOT be truncated.
-					 * 
-					 * [.precision] optional "precision specifier" may be included in a format specifier to indicate the precision with which to convert the data
-					 * 
-					 * [argsize]
-					 * 
-					 * typechar: a single character identifying the conversion type
-					 * 
-					 * ----------copied from above link----------
-					 * 
-					 * [org.apache.hadoop]	StringUtils.rightPad(countryName, 38, "") uses more heap space!
-					 * 		https://commons.apache.org/proper/commons-lang/apidocs/org/apache/commons/lang3/StringUtils.html#rightPad-java.lang.String-int-
-					 */
+				double graduationRateInYear;
 
-					context.write(new Text(String.format("%-38s", countryName)), new DoubleWritable(graduationRateInYear));
+				if (!graduationRateInYearStr.isEmpty()) {
+					graduationRateInYear = Double.parseDouble(graduationRateInYearStr);	
+					
+					if (graduationRateInYear < THRESHOLD) {
+
+						/* pads the output with spaces so it reaches 38 characters for displaying purposes
+						 * NOTE: a tab(\t) counts as 4 spaces in size 
+						 * 
+						 * [java.lang.String]	String.format("%-38s", countryName)
+						 * 		https://docs.oracle.com/javase/7/docs/api/java/lang/String.html#format%28java.lang.String,%20java.lang.Object...%29
+						 * 
+						 * ----------copied from official documentation----------
+						 * 
+						 * Format(ter) String syntax: %[argument_index$][flags][width][.precision]conversion
+						 * 
+						 * [argument_index$] The optional argument_index is a decimal integer indicating the position of the argument in the argument list. 
+						 * 					 The first argument is referenced by "1$", the second by "2$", etc.
+						 * 
+						 * [flags] The optional flags is a set of characters that modify the output format. The set of valid flags depends on the conversion.
+						 * 
+						 * [width] The optional width is a non-negative decimal integer indicating the minimum number of characters to be written to the output.
+						 * 
+						 * [.precision] The optional precision is a non-negative decimal integer usually used to restrict the number of characters. 
+						 * 				The specific behavior depends on the conversion.
+						 * 
+						 * conversion: The required conversion is a character indicating how the argument should be formatted. 
+						 * 		      The set of valid conversions for a given argument depends on the argument's data type. 
+						 * 
+						 * ----------copied from official documentation----------
+						 * 
+						 * 
+						 * 
+						 * System.out.printf syntax: %[flags][width][.precision][argsize]typechar 
+						 * 		https://sharkysoft.com/archive/printf/docs/javadocs/lava/clib/stdio/doc-files/specification.htm
+						 * 
+						 * ----------copied from above link----------
+						 * 
+						 * % indicates that it's a format code  
+						 * [flags] optional prefix; "-" flag left justifies(align left) or pads to the right of the String
+						 * 
+						 * [width] optional "width specifier", if present, indicates the field width, 
+						 * 			or the minimum number of characters in the output that the formatted argument will span. 
+						 * 				-If the string representation of the value does NOT fill the minimum length, the field will be left-padded with spaces. 
+						 * 				-If the converted value exceeds the minimum length, however, the converted result will NOT be truncated.
+						 * 
+						 * [.precision] optional "precision specifier" may be included in a format specifier to indicate the precision with which to convert the data
+						 * 
+						 * [argsize]
+						 * 
+						 * typechar: a single character identifying the conversion type
+						 * 
+						 * ----------copied from above link----------
+						 * 
+						 * [org.apache.hadoop]	StringUtils.rightPad(countryName, 38, "") uses more heap space!
+						 * 		https://commons.apache.org/proper/commons-lang/apidocs/org/apache/commons/lang3/StringUtils.html#rightPad-java.lang.String-int-
+						 */
+
+						context.write(new Text (String.format("%-34s", countryName) + year), new DoubleWritable(graduationRateInYear));
+						year++;
+					}
+				}
 			}
 		}
 	}
